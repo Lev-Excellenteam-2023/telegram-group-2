94b22d9572c3642ac95f9363fa0aee48aac6d164
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-17 11:41:51 +0300

    Changed message

--- Gpt/gpt-session.py
+++ Gpt/gpt-session.py
@@ -6,7 +6,7 @@ from prompts import FIRST_MESSAGES_DICT
 openai.api_key = OPENAI_TOKEN
 
 
-def call_openai_api(message: dict, history: List[dict]=None) -> str:
+def call_openai_api(message: str, history: List) -> str:
     """
     Calls the OpenAI API to generate a response based on the provided question and optional conversation history.
 
@@ -14,11 +14,11 @@ def call_openai_api(message: dict, history: List[dict]=None) -> str:
     :param history: Conversation history (optional).
     :return: A string containing the generated response from the OpenAI API.
     """
+    dict_message = {"role": "user", "content": message}
     history.insert(0, FIRST_MESSAGES_DICT)
-    history.append(message)
+    history.append(dict_message)
     response = openai.ChatCompletion.create(model=CHAT,
                                             messages=history,
                                             max_tokens=MAX_TOKENS)
 
     return response["choices"][0]["message"]["content"]
-
